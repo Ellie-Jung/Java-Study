7954b45f1d19c017737af4356fb26aa555436295
Ellie-Jung <jssoyeon@gmail.com>
2021-05-10 20:36:45 +0900

    Second Week monday

--- MyJavaWorkShop6/src/com/test/Workshop2_3.java
+++ MyJavaWorkShop6/src/com/test/Workshop2_3.java
@@ -2,8 +2,10 @@ package com.test;
  class BBSItem {
 	static int lastSeqNo = 0; 
 	int seqNo; //일련번호 필드
-//	int seqNo = 0;
-//	int seqNo = i++;
+	{
+		++lastSeqNo;
+		seqNo = lastSeqNo;
+	}
 	String writer; //작성자 필드
 	String writtenDate; //작성일자필드
 	String title; //제목필드
@@ -11,7 +13,7 @@ package com.test;
 	
 	BBSItem(){}
 	BBSItem(int seqNo, String writer, String writtenDate, String title, String content){ //생성자
-		this.seqNo = ++lastSeqNo;
+	//	this.seqNo = ++lastSeqNo;
 		this.writer = writer;
 		this.writtenDate = writtenDate;
 		this.title = title;
@@ -22,13 +24,20 @@ public class Workshop2_3 {
 
 	public static void main(String[] args) {
 	
-		BBSItem a = new BBSItem(3,"s","s","g","s");
-		int A =a.seqNo;
-		System.out.println(A);
-		BBSItem b = new BBSItem();
-		int B = b.seqNo;
-		System.out.println(B);
+//		BBSItem a = new BBSItem(3,"s","s","g","s");
+//		int A =a.seqNo;
+//		System.out.println(A);
+//		BBSItem b = new BBSItem();
+//		int B = b.seqNo;
+//		System.out.println(B);
+//		BBSItem c = new BBSItem();
 		
+		BBSItem p1 = new BBSItem();
+		BBSItem p2 = new BBSItem();
+		BBSItem p3 = new BBSItem();
+		System.out.println(p1.seqNo);
+		System.out.println(p2.seqNo);
+		System.out.println(p3.seqNo);
 
 	}
 
