05177eef18263ec380cdf2b9ef40f500f0842ab0
Ellie <jssoyeon@gmail.com>
2021-06-13 00:09:02 +0900

    select ex2 review

--- sql/210612_selectEx2.sql
+++ sql/210612_selectEx2.sql
@@ -14,12 +14,17 @@ where bookid = 1;
 select bookname
 from book
 where price >=20000;
+
 --(3) 박지성의총구매액(박지성의고객번호는1번으로놓고작성)
- 
+select sum(saleprice)
+from orders
+where custid =1;
+
 --(4) 박지성이구매한도서의수(박지성의고객번호는1번으로놓고작성)
---
---​
---
+select count(custid) 
+from orders
+where custid = 1;
+
 --2 마당서점의운영자와경영자가요구하는다음질문에대해SQL 문을작성하시오.
 --
 --(1) 마당서점도서의총개수
