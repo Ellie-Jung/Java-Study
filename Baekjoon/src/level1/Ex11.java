41d6c2bbeb12aca1fd53b1611a141ef90b7e3559
Ellie <jssoyeon@gmail.com>
2021-07-14 01:14:15 +0900

    Update Ex11.java

--- Baekjoon/src/level1/Ex11.java
+++ Baekjoon/src/level1/Ex11.java
@@ -1,10 +1,18 @@
 package level1;
 
+import java.util.Scanner;
+
 public class Ex11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		int a= sc.nextInt();
+		int b= sc.nextInt();
+		
+		
+		
 	}
 
 }
