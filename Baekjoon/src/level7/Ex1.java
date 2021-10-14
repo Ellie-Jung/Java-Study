119fa20d9b5eae305558a0d495df7aa84abaaf79
Ellie <jssoyeon@gmail.com>
2021-10-15 00:04:00 +0900

    Baekjoon 문자열

---
+++ Baekjoon/src/level7/Ex1.java
@@ -0,0 +1,13 @@
+package level7;
+
+import java.util.Scanner;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		
+		String x= scanner.next();
+		char y=x.charAt(0);
+		System.out.println((int)y);
+	}
+}
